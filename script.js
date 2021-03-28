1410c4df54da60c6dd19af147358ef704afa47fc
Rose <r.d.jeanlouis01@gmail.com>
2021-03-28 11:33:25 -0400

    Add CSS rules

---
+++ script.js
@@ -0,0 +1,24 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+const h2 = document.createElement('h2');
+h2.textContent = "This content added by JavaScript";
+document.querySelector('body').appendChild(h2);
+
